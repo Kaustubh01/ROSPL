13f9581252cc5238144e507be7d73838672d3cd5
Kaustubh <kaustubhmayekar02@gmal.com>
2024-08-11 22:44:52 +0530

    First Commit

---
+++ scripts/slime.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+
+@export var speed:float = 1
+var direction =1
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+	position.x += delta*speed*direction
